5b6c2603192f095bb28b4b90bbba7c697b542e75
fauzan388 <fauzanazimm@gmail.com>
2024-07-06 21:02:23 +0700

    a

--- PPPwn/run.sh
+++ PPPwn/run.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /boot/firmware/PPPwn/config.sh ]; then
     # Jika Anda memiliki file config.sh, Anda perlu mengedit nilai-nilai ini di file tersebut bukan di sini
-    INTERFACE="en0" 
+    INTERFACE="eth0" 
     FIRMWAREVERSION="11.00" 
     SHUTDOWN=true
     USBETHERNET=false
