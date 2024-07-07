39e9275216e8b387bc6af3a92691de77a168f557
fauzan388 <fauzanazimm@gmail.com>
2024-07-07 09:04:18 +0700

    a

--- PPPwn/run.sh
+++ PPPwn/run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-VERBOSE=true
-
-log() {
-    if [ "$VERBOSE" = true ]; then
-        echo "$1" | sudo tee /dev/tty1
-    fi
-}
-
-log "Starting script..."
-
 if [ ! -f /boot/firmware/PPPwn/config.sh ]; then
-    log "Config file not found. Using default settings."
     # Jika Anda memiliki file config.sh, Anda perlu mengedit nilai-nilai ini di file tersebut bukan di sini
     INTERFACE="eth0" 
     FIRMWAREVERSION="11.00" 
@@ -20,7 +9,6 @@ if [ ! -f /boot/firmware/PPPwn/config.sh ]; then
     PPPOECONN=false
     USECPP=true
 else
-    log "Loading config file."
     source /boot/firmware/PPPwn/config.sh
 fi
 
@@ -28,8 +16,7 @@ if [[ -z $USECPP ]]; then
     USECPP=true
 fi
 
-PITYP=$(tr -d '\0' </proc/device-tree/model)
-log "Detected device type: $PITYP"
+PITYP=$(tr -d '\0' </proc/device-tree/model) 
 
 if [[ $PITYP == *"Raspberry Pi 2"* ]]; then
     coproc read -t 15 && wait "$!" || true
@@ -46,8 +33,6 @@ else
 fi
 
 arch=$(getconf LONG_BIT)
-log "System architecture: $arch-bit"
-
 if [ $arch -eq 32 ] && [ $CPPBIN = "pppwn64" ]; then
     CPPBIN="pppwn7"
 fi
@@ -66,49 +51,41 @@ ASCII_ART='
 \n'
 
 # Cetak ASCII art ke terminal
-log "Printing ASCII art."
 echo -e "$ASCII_ART" | sudo tee /dev/tty1
 
 # Lanjutkan dengan bagian lain dari skrip Anda
 if [ $USBETHERNET = true ]; then
-    log "Configuring USB Ethernet."
     echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
     coproc read -t 2 && wait "$!" || true
     echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
     coproc read -t 5 && wait "$!" || true
     sudo ip link set $INTERFACE up
 else
-    log "Configuring Ethernet interface."
     sudo ip link set $INTERFACE down
     coproc read -t 5 && wait "$!" || true
     sudo ip link set $INTERFACE up
 fi
 
-log "Device Type: $PITYP"
-log "Ready for console connection"
-log "Firmware: $FIRMWAREVERSION"
-log "Interface: $INTERFACE"
+echo -e "\n\033[36m$PITYP\033[0m\n\033[32mReady for console connection\033[92m\nFirmware:\033[93m $FIRMWAREVERSION\033[92m\nInterface:\033[93m $INTERFACE\033[0m" | sudo tee /dev/tty1
 
 if [ $PPPOECONN = true ]; then
-    log "Internet Access Enabled"
+    echo -e "\033[93mInternet Access Enabled\033[0m" | sudo tee /dev/tty1
 fi
 
 PIIP=$(hostname -I) || true
 if [ "$PIIP" ]; then
-    log "IP: $PIIP"
+    echo -e "\033[92mIP: \033[93m $PIIP\033[0m" | sudo tee /dev/tty1
 fi
 
 while true; do
     if [ $USECPP = true ]; then
-        log "Executing $CPPBIN binary."
         ret=$(sudo /boot/firmware/PPPwn/$CPPBIN --interface "$INTERFACE" --fw "${FIRMWAREVERSION//.}" --stage1 "/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin" --stage2 "/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin")
     else
-        log "Executing Python script."
         ret=$(sudo python3 /boot/firmware/PPPwn/pppwn.py --interface=$INTERFACE --fw=$FIRMWAREVERSION --stage1=/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin --stage2=/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin)
     fi
 
     if [ $ret -ge 1 ]; then
-        log "Console PPPwned!"
+        echo -e "\033[32m\nConsole PPPwned! \033[0m\n" | sudo tee /dev/tty1
 
         if [ $PPPOECONN = true ]; then
             if [ $USBETHERNET = true ]; then
@@ -127,11 +104,10 @@ while true; do
             sudo iptables -t nat -I PREROUTING -p tcp --dport 3232 -j DNAT --to 192.168.2.2:3232
             sudo iptables -t nat -I PREROUTING -p tcp --dport 9090 -j DNAT --to 192.168.2.2:9090
             sudo iptables -t nat -A POSTROUTING -s 192.168.2.0/24 ! -d 192.168.2.0/24 -j MASQUERADE
-            log "PPPoE Enabled"
+            echo -e "\n\n\033[93m\nPPPoE Enabled \033[0m\n" | sudo tee /dev/tty1
             sudo pppoe-server -I $INTERFACE -T 60 -N 1 -C PS4 -S PS4 -L 192.168.2.1 -R 192.168.2.2 -F
         else
             if [ $SHUTDOWN = true ]; then
-                log "Shutting down system."
                 sudo poweroff
             else
                 sudo ip link set $INTERFACE down
@@ -140,7 +116,7 @@ while true; do
 
         exit 1
     else
-        log "Failed retrying..."
+        echo -e "\033[31m\nFailed retrying...\033[0m\n" | sudo tee /dev/tty1
 
         if [ $USBETHERNET = true ]; then
             echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
